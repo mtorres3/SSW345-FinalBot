52ad63ef54b3cb7de76f0d76af5b6b40d6da5df3
Jon Cucci <62709104+joncucci@users.noreply.github.com>
2021-04-10 13:56:25 -0400

    Add files via upload

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,52 +1,27 @@
+# bot.py
+import os
+import random, time
 import discord
-from discord.ext import commands
-import random 
-client = commands.Bot(command_prefix ='_') #instance of bot and set to client variable
+from dotenv import load_dotenv
 
-#bot is online and in ready state
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
 @client.event
 async def on_ready():
-    print('Bot is listening.')
-
-#bot using standard functions  to see if someone joined or left
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined the server')
+    print(f'{client.user} has connected')
 
 @client.event
-async def on_member_remove(member):
-    print(f'{member} has left the server')
+async def on_message(message):
 
-@client.command()
-async def ping(ctx): #command name is function name 
-    await ctx.send(f'Latency: {round(client.latency * 1000)}ms')
+    msg = message.content.lower()
+    msg = msg.split()
 
-@client.command(aliases = ['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ["As I see it, yes.",
-                 "Ask again later.", 
-                 "Better not tell you now.",
-                 "Cannot predict now.",
-                 "Concentrate and ask again.",
-                 "Don’t count on it.", 
-                 "It is certain.","It is decidedly so.",
-                 "Most likely.",
-                 "My reply is no.",
-                 "My sources say no.",
-                 "Outlook not so good.", 
-                 "Outlook good.", 
-                 "Reply hazy, try again.", 
-                 "Signs point to yes.",
-                 "Very doubtful.",
-                 "Without a doubt.",
-                 "Yes.", 
-                 "Yes – definitely.", 
-                 "You may rely on it."]
-    await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
+    if message.author == client.user:
+        return
 
-@client.command()
-async def clear(ctx, amount = 2): #command name is function name 
-    await ctx.channel.purge(limit = amount)
+    elif "!flip" in msg:
+        await message.channel.send(random.choice(['Heads', 'Tails']))
 
-
-client.run('ODMwNDYzNTM3MjkzNDI2Njk4.YHHDcA.zq9lpj0TCRKTcyAsq98Um5beCoU')
+client.run(TOKEN)
