ae9a2cddbf5fb594c2c60e332de63dbc7ca2d306
WilliamBaltus <wbaltus21@gmail.com>
2021-04-12 09:51:57 -0400

    updated bot to include audio playing
    adding pomodoro technique next

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,27 +1,99 @@
-# bot.py
-import os
-import random, time
 import discord
-from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+import os
+from discord.ext import tasks
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
+bot = commands.Bot(command_prefix = '_')
 
-@client.event
+@bot.event 
 async def on_ready():
-    print(f'{client.user} has connected')
+    print("Bot online")
 
-@client.event
-async def on_message(message):
+@bot.command(pass_context = True)
+async def join(ctx):
+    global voice
+    channel =  ctx.message.author.voice.channel #get Vchannel of the one who called balled
+    voice =  get(bot.voice_clients, guild = ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        await channel.connect()
 
-    msg = message.content.lower()
-    msg = msg.split()
+    await ctx.send(f"Joined {channel}")
 
-    if message.author == client.user:
+@bot.command(pass_context = True)
+async def leave(ctx):
+    global voice
+    channel =  ctx.message.author.voice.channel #get Vchannel of the one who called balled
+    voice =  get(bot.voice_clients, guild = ctx.guild)
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        await ctx.send(f"Left {channel}")
+    else:
+        await ctx.send(f"Bot not in {channel}")
+
+#this function works by downloading the youtube video with url provided using youtube_dl dependency
+#this is then converted with FFmpeg dependency so it is in format that can be played
+#this is played by saving the converted audio as "song.mp3" and then playing 
+#if a .mp3 already exists it means that a song is playing, otherwise it would be deleted and remade for next audio
+@bot.command(pass_context = True)
+async def alarm(ctx):  #can do use input as well, input url: str as input and remove hard coded url
+    #if url is None:
+    url = 'https://www.youtube.com/watch?v=LzxCJzM4xLo'
+    
+    global voice
+    voice =  get(bot.voice_clients, guild = ctx.guild)
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+
+    except PermissionError:
+        await ctx.send("Wait for current audio to end!")
         return
+    await ctx.send("Getting audio file...")
 
-    elif "!flip" in msg:
-        await message.channel.send(random.choice(['Heads', 'Tails']))
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        await ctx.send("Downloading audio...")
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    await ctx.send("Done converting, now playing!")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
-client.run(TOKEN)
+@bot.command(pass_context = True)
+async def stop(ctx):
+    voice =  get(bot.voice_clients, guild = ctx.guild)
+    if voice is not None:
+        voice.stop()
+    else:
+        await ctx.send("No audio playing")
+
+@bot.command(pass_context = True)
+async def pause(ctx):
+    voice =  get(bot.voice_clients, guild = ctx.guild)
+    if voice is not None:
+        voice.pause()
+    else:
+        await ctx.send("No audio playing")
+
+@bot.command()
+async def ping(ctx): #command name is function name 
+    await ctx.send(f'Latency: {round(bot.latency * 1000)}ms')
+
+@tasks.loop(seconds=5.0, count=5)
+async def slow_count():
+    print(slow_count.current_loop)
+
+bot.run('TOKEN')
